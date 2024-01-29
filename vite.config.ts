5fbc75719405ee19f60b125f4d3e4ceed337c589
Devin Elrose <hiddnist@gmail.com>
2024-01-28 23:54:28 -0800

    adds readme, base URL, and not awful on mobile

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const baseUrl = process.env.BASE_URL ?? "/"
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: baseUrl.startsWith("/") ? baseUrl : `/${baseUrl}`,
   plugins: [react()],
 })
